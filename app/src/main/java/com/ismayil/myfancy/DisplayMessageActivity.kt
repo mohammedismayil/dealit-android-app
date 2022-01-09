8af67349bace905bb7813615b2cfc9c19547792d
mohammedismayil <mohammedismayil10@gmail.com>
2022-01-09 11:36:03 +0530

    adding image from url

--- app/src/main/java/com/ismayil/myfancy/DisplayMessageActivity.kt
+++ app/src/main/java/com/ismayil/myfancy/DisplayMessageActivity.kt
@@ -1,11 +1,56 @@
 package com.ismayil.myfancy
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.squareup.picasso.Picasso;
+//import android.R
+import android.widget.ImageView
+
 
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+
+        val imageView: ImageView = findViewById(R.id.imageView3)
+
+//        Picasso.get().load("https://media.geeksforgeeks.org/wp-content/cdn-uploads/logo-new-2.svg").into(imageView)
+
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("DisplayMessageActivity", "onStart called")
+    }
+
+    override fun onResume() {
+
+
+        super.onResume()
+        Log.d("DisplayMessageActivity", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("DisplayMessageActivity", "onPause called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d("DisplayMessageActivity", "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("DisplayMessageActivity", "onDestroy called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("DisplayMessageActivity", "onRestart called")
     }
 }
