08d8f660f2e63a7746278e622510332a3eff6e7d
mohammedismayil <mohammedismayil10@gmail.com>
2022-01-19 23:49:59 +0530

    new login screen added

---
+++ app/src/main/java/com/ismayil/myfancy/FoodLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.ismayil.myfancy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_login)
+    }
+}
