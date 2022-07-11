7113ddedd79322880d409321b9dd6d1caed2ba75
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-07-11 10:01:35 +0530

    changes

--- app/src/main/java/com/ismayil/myfancy/FoodLoginActivity.kt
+++ app/src/main/java/com/ismayil/myfancy/FoodLoginActivity.kt
@@ -1,11 +1,33 @@
 package com.ismayil.myfancy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+//import wallet.core.jni.HDWallet
 class FoodLoginActivity : AppCompatActivity() {
+
+    init {
+        System.loadLibrary("TrustWalletCore")
+    }
+    private val seedPhrase = "ripple scissors kick mammal hire column oak again sun offer wealth tomorrow wagon turn fatal"
+    private val passphrase = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_login)
+//        val wallet = HDWallet(seedPhrase, passphrase)
+
+
+        // get reference to button
+        val btn_click_me = findViewById(R.id.button_login) as Button
+// set on-click listener
+        btn_click_me.setOnClickListener {
+            Toast.makeText(this,"button 1 clicked", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/ismayil/myfancy/HomeScreenActivity.kt
@@ -0,0 +1,40 @@
+package com.ismayil.myfancy
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.ismayil.myfancy.databinding.ActivityHomeScreenBinding
+
+class HomeScreenActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityHomeScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_home_screen)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_home_screen)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
